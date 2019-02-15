50a0e638c375184a254b99b789bf5296f87e5cd7
Gabriel Haines <gabrielhaines@hotmail.com>
2019-02-15 23:27:46 +1100

    part 1/ create basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import CreateTodo from "./components/Create-todo";
+import EditTodo from "./components/Edit-todo";
+import TodosList from "./components/Todos-list";
+
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+      <img src={logo} width="30" height="30" alt="react logo" />
+
+      <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+      <div className="collpase navbar-collapse">
+      <ul className="navbar-nav mr-auto">
+      <li className="navbar-item">
+      <Link to="/" className="nav-link">Todos</Link>
+      </li>
+      <li className="navbar-item">
+      <Link to="/create" className="nav-link">Create Todo</Link>
+      </li>
+      </ul>
       </div>
+      </nav>
+      <br/>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+      </div>
+      </Router>
+
     );
   }
 }
